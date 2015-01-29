6f36a3810a70523293bbe793e7a232dace1cb19f
Gordon Fierce <gordon.fierce@gmail.com>
2015-01-29 18:33:41 -0500

    Simplified the accelerate code. Turns out it wasn't broken.

--- test_car.py
+++ test_car.py
@@ -3,16 +3,16 @@ from car import Car
 def test_accelerate():
     car = Car()
     car.speed = 20
-    car.speed = car.accelerate()
+    car.accelerate()
     assert car.speed == 22
 
     car2 = Car()
     car2.speed = 32
-    car2.speed = car2.accelerate()
+    car2.accelerate()
     assert car2.speed == 33.33
 
 def test_decelerate():
     car = Car()
     car.speed = 20
-    car.speed = car.decelerate()
+    car.decelerate()
     assert car.speed == 18

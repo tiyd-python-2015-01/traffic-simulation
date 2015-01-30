86809b97423c1830ac49869ffce4eddb218e23b2
Alan Grissett <alan.grissett@gmail.com>
2015-01-30 06:40:44 -0500

    Added very basic road with docstrings

---
+++ road.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Road:
+    """The Road class is a storage object that will contain all of the
+    cars created by the simulation.  The road may have a modifer that will
+    indicate the difficulty of driving on this section of road and will
+    be used to calculate the chance of slowdown by drivers throughout that
+    section of road.
+
+    Responsibilities:
+    * Contains a list where each element in the list corresponds to 1 meter
+      of road
+    * Has a modifier variable that will be used to modify the slowdown chance
+      of each car contained by the road object."""
+
+
+    def __init__(self, modifier=1):
+        self.road = np.zeros(1000)
+        self.modifier = modifier
+
+    def get_road_modifier(self):
+        """Returns the slowdown modifier for this road object.  Default is 1"""
+        return self.modifier
